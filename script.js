8500a355bd0f68150ab543c27d88e2811f5db06a
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-05-22 10:44:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let elements = document.getElementsByTagName("strong")
 function highlight() {
     //Write your code here
+	for(let i=0; i<elements.length; i++){
+		elements[i].style.color = "green"
+	}
 
 
 }
@@ -7,6 +11,8 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
-
+	for(let i=0;i<elements.length;i++){
+		elements[i].style.color = "black"
+	}
     
 }
